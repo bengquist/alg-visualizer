bd0505082ce22ffb2e0f61d86b89ee1fbaca72a5
bengquist <engquist_blake@yahoo.com>
2020-03-29 19:38:48 -0500

    initialize project and add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-800">
+        <select className="appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          <option>Bubble</option>
+        </select>
+        <button className="p-6 p-4">hey</button>
+      </div>
     </div>
   );
 }
