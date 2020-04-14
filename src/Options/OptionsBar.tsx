f2eb7bb7acb8986d959a77e2c17142e10bf3b063
bengquist <engquist_blake@yahoo.com>
2020-04-13 23:26:42 -0500

    added font awesome icons

--- src/Options/OptionsBar.tsx
+++ src/Options/OptionsBar.tsx
@@ -1,15 +1,40 @@
+import {
+  faBackward,
+  faForward,
+  faPlay,
+  faUndo,
+  faVolumeMute,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function OptionsBar() {
   return (
-    <div className="rounded overflow-hidden shadow-lg bg-gray-800 flex flex-row justify-between">
+    <div className="rounded overflow-hidden shadow-lg bg-gray-800 flex flex-row justify-between p-4 text-white">
       <select className="bg-gray-200 border border-gray-200 text-gray-700 px-4 pr-8 focus:outline-none focus:bg-white focus:border-gray-500">
         <option>Bubble</option>
       </select>
-      <button className="p-6 p-4">hey</button>
+
+      <div className="flex">
+        <button className="p-3">
+          <FontAwesomeIcon icon={faUndo} />
+        </button>
+        <button className="p-3">
+          <FontAwesomeIcon icon={faBackward} />
+        </button>
+        <button className="p-3">
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+        <button className="p-3">
+          <FontAwesomeIcon icon={faForward} />
+        </button>
+        <button className="p-3">
+          <FontAwesomeIcon icon={faVolumeMute} />
+        </button>
+      </div>
+
       <input type="range" />
       <input type="range" />
-      <button className="p-6 p-4">ayylmao</button>
     </div>
   );
 }
